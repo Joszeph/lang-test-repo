509a1246882de69d224c0dbb4f952cbf6a7be1ba
Yosif Georgiev <iodesign@live.com>
2021-05-20 17:41:03 +0300

    add post

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Toolbar } from '../components/toolbar';
-import imageUrlBuilder from '@sanity/image-url';
+// import imageUrlBuilder from '@sanity/image-url';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
@@ -17,16 +17,16 @@ export default function Home({ posts }) {
 
   useEffect(() => {
     if (posts.length) {
-      const imgBuilder = imageUrlBuilder({
-        projectId: 'mjoyrhci',
-        dataset: 'production',
-      });
+      // const imgBuilder = imageUrlBuilder({
+      //   projectId: 'mjoyrhci',
+      //   dataset: 'production',
+      // });
 
       setMappedPosts(
         posts.map(p => {
           return {
             ...p,
-            mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
+            // mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
           }
         })
       );
@@ -47,7 +47,7 @@ export default function Home({ posts }) {
           {mappedPosts.length ? mappedPosts.map((p, index) => (
             <div onClick={() => router.push(`/post/${p.slug.current}`)} key={index} className={styles.post}>
               <h3>{p.title}</h3>
-              <img className={styles.mainImage} src={p.mainImage} />
+              {/* <img className={styles.mainImage} src={p.mainImage} /> */}
             </div>
           )) : <>No Posts Yet</>}
         </div>
@@ -58,7 +58,7 @@ export default function Home({ posts }) {
 
 export const getServerSideProps = async pageContext => {
   const query = encodeURIComponent('*[ _type == "post" ]');
-  const url = `https://mjoyrhci.api.sanity.io/v1/data/query/production?query=${query}`;
+  const url = `https://kna6xtr1.api.sanity.io/v1/data/query/production?query=${query}`;
   const result = await fetch(url).then(res => res.json());
 
   if (!result.result || !result.result.length) {
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,27 +1,27 @@
-import imageUrlBuilder from '@sanity/image-url';
+// import imageUrlBuilder from '@sanity/image-url';
 import { useState, useEffect } from 'react';
 import styles from '../../styles/Post.module.css';
 import BlockContent from '@sanity/block-content-to-react';
 import { Toolbar } from '../../components/toolbar';
 
-export const Post = ({ title, body, image }) => {
-  const [imageUrl, setImageUrl] = useState('');
+export const Post = ({ title, body, image}) => {
+  // const [imageUrl, setImageUrl] = useState('');
 
-  useEffect(() => {
-    const imgBuilder = imageUrlBuilder({
-      projectId: 'mjoyrhci',
-      dataset: 'production',
-    });
+  // useEffect(() => {
+  //   const imgBuilder = imageUrlBuilder({
+  //     projectId: 'mjoyrhci',
+  //     dataset: 'production',
+  //   });
 
-    setImageUrl(imgBuilder.image(image));
-  }, [image]);
+  //   setImageUrl(imgBuilder.image(image));
+  // }, [image]);
 
   return (
     <div>
       <Toolbar />
       <div className={styles.main}>
         <h1>{title}</h1>
-        {imageUrl && <img className={styles.mainImage} src={imageUrl} />}
+        {/* {imageUrl && <img className={styles.mainImage} src={imageUrl} />} */}
 
         <div className={styles.body}>
           <BlockContent blocks={body} />
@@ -41,7 +41,7 @@ export const getServerSideProps = async pageContext => {
   }
 
   const query = encodeURIComponent(`*[ _type == "post" && slug.current == "${pageSlug}" ]`);
-  const url = `https://mjoyrhci.api.sanity.io/v1/data/query/production?query=${query}`;
+  const url = `https://kna6xtr1.api.sanity.io/v1/data/query/production?query=${query}`;
 
   const result = await fetch(url).then(res => res.json());
   const post = result.result[0];
@@ -55,7 +55,7 @@ export const getServerSideProps = async pageContext => {
       props: {
         body: post.body,
         title: post.title,
-        image: post.mainImage,
+        // image: post.mainImage,
       }
     }
   }
